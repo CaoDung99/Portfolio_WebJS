6a93d781e1110a26e17ef97af37af858d2e20860
CaoDung99 <91966835+CaoDung99@users.noreply.github.com>
2022-02-16 14:27:07 +0700

    done

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "",
+    link: "http://lama.dev",
+  },
+];
