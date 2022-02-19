fdf84102b15237907932ce686647af443782995d
CaoDung99 <91966835+CaoDung99@users.noreply.github.com>
2022-02-19 23:21:04 +0700

    update date

--- src/data.js
+++ src/data.js
@@ -1,7 +1,8 @@
+
 export const products = [
   {
     id: 1,
-    img: "/client/src/img/AdminLTE.jpg",
+    img: "https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-6/273868092_3209389449382961_5670068709318012997_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=730e14&_nc_ohc=tFl0mvImGQUAX-5ehgG&tn=6SaPd-s45AVIHUSf&_nc_ht=scontent.fsgn5-6.fna&oh=00_AT8LXumCPzhdIggSyTY2bfOSqw-luKh_mpyKJiRUDGLGTg&oe=62150DCE",
     link: "https://pensive-edison-b1cf7b.netlify.app/",
   },
   {
